30ff813fed19f12e3e333eadb26809944af09832
Kevin Roa <kevinroa101@gmail.com>
2020-10-17 19:01:28 -0400

    File error handling

--- A05/DB05.py
+++ A05/DB05.py
@@ -4,20 +4,30 @@
 # Usage (In shell):
 #   1. python3 DB05.py
 
-#	Get file name from user
-fileName = input("Enter a file name: ")
+import sys
 
-# Initialize counters
-wordCount = sentenceCount = 0
+def main():
+	#	Get file name from user
+	fileName = input("Enter a file name: ")
 
-# Open file
-with open(fileName, 'r') as file:
-	# Loop through every line in file
-	for line in file:
-		# Split the line on every word and get the count
-		wordCount += len(line.split())
-		# Count every occurance of .?! in the line and sum them up
-		sentenceCount += sum([line.count(x) for x in ['.', '?', '!']])
+	# Initialize counters
+	wordCount = sentenceCount = 0
 
-# Print the word and sentence count
-print(fileName, 'has', wordCount,'words and', sentenceCount, 'sentences.')
+	# Catch error if invalid file name
+	try:
+		# Open file
+		with open(fileName, 'r') as file:
+			# Loop through every line in file
+			for line in file:
+				# Split the line on every word and get the count
+				wordCount += len(line.split())
+				# Count every occurance of .?! in the line and sum them up
+				sentenceCount += sum([line.count(x) for x in ['.', '?', '!']])
+	except FileNotFoundError as err:
+		# Print error on invalid file name and exit program
+		sys.exit(err)
+
+	# Print the word and sentence count
+	print(fileName, 'has', wordCount,'words and', sentenceCount, 'sentences.')
+
+main()
